5ae64d77e5cc2c4f672ddced70b91cdf796b707a
AlexOutis <112819420+AlexOutis@users.noreply.github.com>
2022-09-16 13:39:58 +0200

    Update pl.py

--- pl.py
+++ pl.py
@@ -8,14 +8,14 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 cos = np.cos;sin = np.sin;tan = np.tan
 arcsin = np.arcsin;arccos = np.arccos; pi = math.pi; floor = math.floor
-params = np.empty((0, 6))
-filename = input("Input file's name: ")
+
+filename = input("Your file with orbital elements: ")
 f = open(filename, 'r')
+params = np.empty((0, 6))
 
 def addParams(params, ln):
-    e, i, a, w, om = map(float, ln.split()[:5])
-    col = ln.split()[5]
-    params = np.append(params, np.array([[e, i, a, w, om, col]]), axis=0)
+    newParams= ln.split()
+    params = np.append(params, np.array([newParams]), axis=0)
     return params
 ln = f.readline()
 
@@ -25,32 +25,25 @@ while ln:
     
 dots = []
 years = int(input("Choose animation duration (in years, for example 10): "))
-animDur = math.floor(years * 365.25)
+animDur = floor(years * 365.25)
 animSpeed = 10
-animFrames = math.floor(animDur / animSpeed)
+animFrames = floor(animDur / animSpeed)
 gm = 39.4784176
+
 def drdt(u, t):
     temp = -gm/((u[0]**2+u[1]**2+u[2]**2)**1.5)
     return (u[3], u[4], u[5], temp*u[0], temp*u[1], temp*u[2])
-   
 
-def angle(sinx, cosx):
-    return math.atan2(sinx, cosx)
-
-def calcE(M, e):
-    M = M%(2 * pi)
+def iterE(M, e):
     E = M
     for i in range(8):
         E = M + e * sin(E) 
     return E
 
 def updateRF(a, e, t, Per):
-    Per = Per * 365.25
     M = 6.2831853/Per * t
-    E_eSinE = M
-    E = calcE(M, e)
-    cosE = cos(E)
-    rnow = a*(1 - e *cosE)
+    E = iterE(M, e)
+    rnow = a*(1 - e *cos(E))
     f = 2*np.arctan(math.sqrt((1+e)/(1-e)*tan(E/2)))
     return (rnow, f)
 
@@ -63,13 +56,12 @@ def drawOrbit(e, i, a, w, om, col):
     inc = 2 *pi/1000
     x, y, z = [np.empty(1000) for i in range(3)]
     for i in range(1000):
-        cosf = cos(f)
-        r = p/(1 + e*cosf)
+        r = p/(1 + e*cos(f))
         sinb = sini * sin(w + f)
         tanlom = cosi * tan(w + f)
         lom = np.arctan(tanlom)
-        if (sin(lom)* sin(w+f)) <  0:
-            lom += pi
+        if (sin(lom)* cosi * sin(w+f)) <  0:
+           lom += pi
         l = lom + om
         b = arcsin(sinb)
 
@@ -79,31 +71,24 @@ def drawOrbit(e, i, a, w, om, col):
         f += inc
 
     ax.plot(x, y, z, col)
-    
-def updateVect(pos, vel, dt):
-    temp = gm/((pos[0]**2 + pos[1]**2+pos[2]**2)**1.5)
-    acc = -temp * pos
-    pos += vel * dt
-    vel += acc * dt
-    return (pos, vel)
 
 def initPos(r, a, i, w, om):
     cosi = cos(i); sini = sin(i)
-    pos = np.empty(3)
+    pos, vel = [np.empty(3) for _ in range(2)]
     sinw = sin(w); cosw= cos(w)
     sinom = sin(om); cosom=cos(om)
-    sinb = sini * math.sin(w)
+    sinb = sini * sin(w)
     tanlom = cosi * tan(w)
     lom = np.arctan(tanlom)
-    if (sin(lom)* sin(w)) <  0:
+    if (sin(lom)* cosi*sin(w)) <  0:
         lom += pi
     l = lom + om
-    b = np.arcsin(sinb)
+    b = arcsin(sinb)
 
     pos[0] = r * cos(b)*sin(l)
     pos[1] = r * cos(b)*cos(l)
     pos[2] = r * sin(b)
-    vel = np.empty(3)
+
     velMag = math.sqrt(gm*(2/r - 1/a))
     vel[1] = (-cosom*sinw - sinom*cosw*cosi)*velMag
     vel[0] = (-sinom*sinw+cosom*cosw*cosi)*velMag
@@ -115,17 +100,12 @@ def calcPos(e, i, a, w, om, dt):
     i, w, om = map(math.radians, [i, w, om])
     Per = a**1.5
     sini = sin(i); cosi = cos(i)
-    t = 0
-    dt = dt/365.25
-    r, f = updateRF(a, e, t, Per)
-    pos, vel = [np.empty(0) for _ in range(2)]
+    r, f = updateRF(a, e, 0, Per)
     pos, vel = initPos(r, a, i, w, om)    
     
-    tarr = np.arange(0, 10, 10/animFrames)
     y0 =  [*pos, *vel]
-    loc = odeint(drdt, y0, tarr)
+    loc = odeint(drdt, y0, np.arange(0, 10, 10/animFrames))
     x = loc[:,0]; y = loc[:,1];z=loc[:,2]
-    
     return [x, y, z]
     
 def init_dots():
@@ -146,25 +126,22 @@ def maxLim():
     return x
 
 ax.plot([0], [0], [0], 'yo')
-maxDist = maxLim()*1.3
-ax.set_xlim(-maxDist, maxDist)
-ax.set_ylim(-maxDist, maxDist)
-ax.set_zlim(-maxDist, maxDist)
-
-#ax.grid(False)
-#plt.axis('off')	
-
 init_dots()
-
 coords = np.empty([0, 3, animFrames])
 
 for param in params:
     drawOrbit(*param)
-    temp = np.array(calcPos(*param[:5], animSpeed))
+    temp = calcPos(*param[:5], animSpeed)
     temp = np.reshape(temp, (1, 3, animFrames))
     coords = np.append(coords, temp, axis=0)
 
-ax.set_title('Visualisation of orbits')
+tempx = ax.get_xlim()
+tempz = ax.get_zlim()
+N = (tempz[1]-tempz[0])/(tempx[1]-tempx[0])
+ax.set_box_aspect((1, 1, N))
+ax.grid(False)
+plt.axis('off')	
 
+ax.set_title('Visualisation of orbits')
 myAnimation = animation.FuncAnimation(fig, animate, frames = animFrames, interval = 40, repeat=True)
 plt.show()
